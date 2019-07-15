faeed03aef3f35a0241174130a35191f55ef053a
André Luiz <andreluizr@gmail.com>
2019-07-14 22:04:39 -0300

    teste mark

--- react-0/src/components/RecipeItem.js
+++ react-0/src/components/RecipeItem.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const RecipeItem = (props) => {
 
-let texto = props.titulo.split(''); 
+let texto = props.titulo.split('');
+
+const tituloMark = texto.map(text => {if (text.toLowerCase() === props.searchString.toLowerCase()) { return <mark> +text </mark>} return text}) ;
+console.log(tituloMark);
+//((tituloMark,[chave,valor]) => (tituloMark = valor === props.searchString ? <mark> +Valor +</mark> : valor))}
+
+//console.log(titulo);
+
 console.log('SEARCH STRING: ',props.searchString);
 
   return (
@@ -16,7 +23,7 @@ console.log('SEARCH STRING: ',props.searchString);
         />
 
         <div className="card-body">
-          <h5 className="card-title"> {texto.map = (text,index) => (text === props.searchString ? <mark> +text +</mark> : text )}</h5>
+          <h5 className="card-title">{tituloMark} </h5>
           <p className="card-text">
             <strong>Ingredients: </strong><div dangerouslySetInnerHTML={{ __html: props.ingrediente }} />
           </p>
