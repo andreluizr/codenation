19f68c7ac929cef54e1277dc389aadc678fabddd
André Luiz <andreluizr@gmail.com>
2019-07-14 21:06:37 -0300

    React-0
    Exercicio - Falta arrumar <mark>

---
+++ react-0/src/components/RecipeItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const RecipeItem = (props) => {
+
+let texto = props.titulo.split(''); 
+console.log('SEARCH STRING: ',props.searchString);
+
+  return (
+    
+    <div className="col-sm-3 mt-4">
+      <div className="card">
+        <img
+          className="card-img-top img-fluid"
+          src= {props.imagem} //"https://via.placeholder.com/350x300"
+          alt=""
+        />
+
+        <div className="card-body">
+          <h5 className="card-title"> {texto} </h5>
+          <p className="card-text">
+            <strong>Ingredients: </strong><div dangerouslySetInnerHTML={{ __html: props.ingrediente }} />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeItem;
