f50be41916740403c2130fe190e4e2a41696ed03
André Luiz <andreluizr@gmail.com>
2019-07-17 17:55:14 -0300

    arrumando o exercicio

--- react-0/src/components/App.js
+++ react-0/src/components/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar'
-import RecipeItem from './RecipeItem'
-import recipes from '../sample_data/recipes.json'
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import RecipeItem from "./RecipeItem";
+import recipes from "../sample_data/recipes.json";
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.recipes = recipes.results;
-    this.state = {
-      searchString: ''
-    };
+    this.state = { searchString: "" };
   }
 
-  render() { 
+  addSearchString = props => {
+    this.setState({ searchString: props });
+  };
+
+  receitasFiltradas = props => {
+    return props.filter(
+      receita =>
+        receita.ingredients.search(new RegExp(this.state.searchString, "i")) >
+          -1 ||
+        receita.title.search(new RegExp(this.state.searchString, "i")) > -1 ||
+        this.state.searchString.length === 0
+    );
+  };
+
+  render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar addSearchString={this.addSearchString} />
         <div className="container mt-10">
           <div className="row">
-            <RecipeItem />
-            <RecipeItem />
-            <RecipeItem />
-            <RecipeItem />
+            {this.receitasFiltradas(this.recipes).map((receita,index) => (
+              <RecipeItem
+                key={index}
+                titulo={receita.title}
+                ingrediente={receita.ingredients}
+                imagem={receita.thumbnail}
+                searchString={this.state.searchString}
+              />
+            ))}
           </div>
         </div>
       </div>
--- react-0/src/components/RecipeItem.js
+++ react-0/src/components/RecipeItem.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const RecipeItem = () => (
+const RecipeItem = props => {
+  const tituloMark = props.titulo;
+
+  const ingredienteMark = props.ingrediente
+    .split()
+    .map(ingrediente =>
+      ingrediente.toLowerCase().includes(props.searchString.toLowerCase()) ? (
+        <mark>{ingrediente}</mark>
+      ) : (
+        { ingrediente }
+      )
+    );
+
+  //props.ingrediente.split(this).map(ingrediente => (<mark>{ingrediente}</mark>));
+
+  console.log({ ingredienteMark });
+
+  return (
     <div className="col-sm-3 mt-4">
-        <div className="card">
-            <img className="card-img-top img-fluid" src="https://via.placeholder.com/350x300" alt="" />
-            <div className="card-body">
-                <h5 className="card-title">TITLE HERE</h5>
-                <p className="card-text">
-                    <strong>Ingredients: </strong>INGREDIENTS HERE
-                </p>
-            </div>
+      <div className="card">
+        <img
+          className="card-img-top img-fluid"
+          src={props.imagem} //"https://via.placeholder.com/350x300"
+          alt=""
+        />
+
+        <div className="card-body">
+          <h5 className="card-title">{tituloMark} </h5>
+          <p className="card-text">
+            <strong>Ingredients: </strong>
+            {ingredienteMark}
+          </p>
         </div>
+      </div>
     </div>
-)
+  );
+};
 
 export default RecipeItem;
